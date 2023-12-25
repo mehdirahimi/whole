346e1dd9e8393854b919853a9ca9c484c46884f0
mehdirahimi <mehdirahimi@email.com>
2023-12-25 14:33:23 +0330

    Initial commit.

---
+++ configserver/src/test/java/ir/smia/configserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.smia.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
