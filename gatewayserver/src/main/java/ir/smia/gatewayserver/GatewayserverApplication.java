9f8df8f557e02fcfdf6349d3ec61a3f3e0368f60
Mehdi Rahimi <mehdirahimi@email.com>
2024-04-01 00:04:14 +0330

    Define Eureka and config servers in Gateway project.

--- gatewayserver/src/main/java/ir/smia/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/ir/smia/gatewayserver/GatewayserverApplication.java
@@ -2,7 +2,9 @@ package ir.smia.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayserverApplication {
 
